0e8c6231bd74ec09b4679bd2e4c11e0f2dfda5cc
Artur <rodriguesartur47@gmail.com>
2024-08-06 09:26:17 -0300

    Implemented PerformNetworkRequestsConcurrentlyViewModel.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -8,14 +8,24 @@ class PerformNetworkRequestsConcurrentlyViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        executeLaunchCoroutineRequest {
+            val oreoFeature = mockApi.getAndroidVersionFeatures(27)
+            val pieFeature = mockApi.getAndroidVersionFeatures(28)
+            val android10Feature = mockApi.getAndroidVersionFeatures(29)
+
+            val versionFeature = listOf(oreoFeature, pieFeature, android10Feature)
+
+            uiState.value = UiState.Success(versionFeature)
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
-
+        uiState.value = UiState.Loading
     }
 
     override fun setError(messageError: String) {
-        TODO("Not yet implemented")
+        uiState.value = UiState.Error(messageError)
     }
 }
